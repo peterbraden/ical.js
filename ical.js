1ebea907d9560a0704007e90dd811d1ca30192d3
Peter Braden <peterbraden@peterbraden.co.uk>
2011-03-10 22:02:46 +0000

    ICal parsing

--- ical.js
+++ ical.js
@@ -8,20 +8,85 @@
 var request = require('request')
 
 
+
+var storeParam = function(name){
+	return function(val, params, curr){
+		if (params && params.length && !(params.length==1 && params[0]==='CHARSET=utf-8')){
+			curr[name] = {params:params, val:val}
+		}
+		else
+			curr[name] = val
+
+		return curr
+	}	
+}	
+	
+
+exports.objectHandlers = {
+	'BEGIN' : function(component, params, curr){
+			if (component === 'VCALENDAR')
+				return curr;
+			return {type:component, params:params}
+		}
+
+  , 'END' : function(component, params, curr, par){
+	  if (curr.uid)
+			par[curr.uid] = curr		
+  }
+
+  , 'SUMMARY' : storeParam('summary')
+  , 'UID' : storeParam('uid')
+  , 'LOCATION' : storeParam('location')
+  , 'DTSTART' : storeParam('start')
+  , 'DTEND' : storeParam('end')
+	
+}	
+
+exports.handleObject = function(name, val, params, stack, par, line){
+	if(exports.objectHandlers[name])
+	  return exports.objectHandlers[name](val, params, stack, par, line)
+	
+	return stack	  
+}	
+
+
+
 exports.parseICS = function(str){
   var lines = str.split('\r\n')
+	, kv, value, params, name, kp
+	, out = {}
+    , ctx = {}
 
   for (var i = 0, ii = lines.length, l = lines[0]; i<ii; i++, l=lines[i]){
     //Unfold : RFC#3.1
     if (lines[i+1] && /\s/.test(lines[i+1][0])){
-	  l += lines[i+1]
+	  l += lines[i+1] // TODO - strip leading whitespace
 	  i += 1		
 	}	
 
-    console.log(i, l)
+	kv = l.split(":")
+	
+	if (kv.length != 2){
+	  // Invalid line - must have k&v
+	  continue;
+	}	
+		
+	value = kv[1]
+	
+	kp = kv[0].split(";")
+	name = kp[0]
+	params = []
+	
+	if (kp.length > 1){
+	  for (var pi = 1; pi < kp.length; pi++){
+	    params.push(kp[pi]);
+	  }	  
+	}
+
+	exports.handleObject(name, value, params, ctx, out, l)  
   }	  
 
-  return {}
+  return out
 }	
 
 exports.fromUrl = function(url, opts, cb){
@@ -35,6 +100,6 @@ exports.fromUrl = function(url, opts, cb){
   })
 }	
 
-exports.fromUrl('http://lanyrd.com/topics/nodejs/nodejs.ics', {}, function(){})
+//exports.fromUrl('http://lanyrd.com/topics/nodejs/nodejs.ics', {}, function(err, data){console.log("OUT:", data)})
 
 
