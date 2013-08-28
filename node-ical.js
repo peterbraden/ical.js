bbdac8900edac3c0ae1f5772742e0efa92027bef
Stuart Young <stuart@goba.mobi>
2013-08-28 13:09:45 -0500

    Try creating instance var.

--- node-ical.js
+++ node-ical.js
@@ -17,11 +17,11 @@ exports.parseFile = function(filename){
   return ical.parseICS(fs.readFileSync(filename, 'utf8'))
 }
 
-var rrule = require('rrule');
+var RRule = require('rrule').RRule;
 
 
 ical.objectHandlers['RRULE'] = function(val, params, curr, par, line){
-  var instance = new rrule.RRule({},false);
+  var instance = new RRule();
   curr['rrule'] = instance.fromString(line.replace("RRULE:", ""));
   return curr
 }
