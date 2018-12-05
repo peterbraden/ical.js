e20ca9cb0d3fbc178cbb54ed3299a157a68db89b
Jonathan New <jonnew@gmail.com>
2018-12-05 11:19:28 -0800

    Adding a dateOnly property when only the date is given. Fixes #94

--- ical.js
+++ ical.js
@@ -115,6 +115,7 @@
           );
 
           newDate = addTZ(newDate, params);
+          newDate.dateOnly = true;
 
           // Store as string - worst case scenario
           return storeValParam(name)(newDate, curr)
